219ae569db8a255f54c1b229e1a3d51a5738caba
dmitrygvl <dmitrygv1994@ya.ru>
2024-03-29 14:30:58 +0300

    Add lokeApi & followApi

---
+++ src/services/likeApi.ts
@@ -0,0 +1,26 @@
+import { api } from './api';
+import type { Like } from '../app/types';
+
+export const likeApi = api.injectEndpoints({
+  endpoints: builder => ({
+    likePost: builder.mutation<Like, { postId: string }>({
+      query: likeData => ({
+        url: '/likes',
+        method: 'POST',
+        body: likeData,
+      }),
+    }),
+    unlikePost: builder.mutation<void, string>({
+      query: postId => ({
+        url: `/likes/${postId}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
+});
+
+export const { useLikePostMutation, useUnlikePostMutation } = likeApi;
+
+export const {
+  endpoints: { likePost, unlikePost },
+} = likeApi;
