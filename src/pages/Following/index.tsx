0f5427c0b60396e5904cea458210d947fb6e4556
dmitrygvl <dmitrygv1994@ya.ru>
2024-04-04 14:49:12 +0300

    Add Followers & Following pages

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,7 +1,35 @@
-import type { FC } from 'react';
+import { type FC } from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardBody } from '@nextui-org/react';
+import { useAppSelector } from '../../app/hooks';
+import { selectCurrentUser } from '../../features/user/userSlice';
+import User from '../../components/User';
 
 const Following: FC = () => {
-  return <div>Following</div>;
+  const currentUser = useAppSelector(selectCurrentUser);
+
+  if (!currentUser) {
+    return null;
+  }
+  return currentUser.following.length > 0 ? (
+    <div className="gap-5 flex flex-col">
+      {currentUser.following.map((user) => (
+        <Link to={`/users/${user.following.id}`} key={user.following.id}>
+          <Card>
+            <CardBody className="blok">
+              <User
+                name={user.following.name ?? ''}
+                avatarUrl={user.following.avatarUrl ?? ''}
+                description={user.following.email ?? ''}
+              />
+            </CardBody>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  ) : (
+    <p className="text-center">У вас пока нет подписок</p>
+  );
 };
 
 export default Following;
