14abd2471ef837f554ea0d29a1f00194e7898646
dmitrygvl <dmitrygv1994@ya.ru>
2024-04-13 19:11:17 +0300

    Fix Dpckerfile

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,3 @@
-// export const BASE_URL =
-//   process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000';
-
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
     ? 'http://localhost:3000'
